1be9bf00c446ff4edb7a3552350885527f04f1e9
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-12-26 06:46:02 +0200

    додає скрипт для форми, який зберігає значення полів

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formRefs = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state'; // ключ для сховища
+const formData = {} 
+
+formRefs.addEventListener('input', throttle(onFormData, 500));
+formRefs.addEventListener('submit', onFormSubmit);
+
+SaveData();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+formData.email = formRefs.elements.email.value;
+formData.message = formRefs.elements.message.value;
+console.log(formData);
+
+formRefs.reset();
+
+localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+}
+
+function SaveData() {
+    let savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        const parceSaveData = JSON.parse(savedData);
+
+        for (const prop in parceSaveData) {
+            if (parceSaveData.hasOwnProperty(prop)) {
+                formRefs.elements[prop].value = parceSaveData[prop];
+                formData[prop] = parceSaveData[prop];
+            }
+    }
+}
+}
