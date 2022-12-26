9efacf30bd6348c74a53f239c6b61f4496f13c3e
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-12-26 06:08:34 +0200

    додає 2 завдання, відеоплеєр

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }
+  
+  const getTime = (localStorage.getItem('videoplayer-current-time'));
+  
+  if (getTime !== null) {
+    player.setCurrentTime(getTime)
+  }
